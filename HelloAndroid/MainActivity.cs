3aa4bacbc73e29fdb916cfbb82c0e87722d4dca5
lichen <1010043642@qq.com>
2019-03-06 18:01:53 +0800

    添加项目文件。

---
+++ HelloAndroid/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+
+    public class MainActivity : AppCompatActivity
+    {
+
+        int count = 0;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // 加载布局
+            SetContentView(Resource.Layout.activity_main);
+            // 获取布局中的控件
+            Button say = FindViewById<Button>(Resource.Id.buttonPanel);
+            TextView show = FindViewById<TextView>(Resource.Id.text);
+            // 绑定 Click 事件
+            say.Click += (sender, e) =>
+            {
+                count++;
+                show.Text = "Hello, Android";
+                say.Text = $"You Clicked {count}";
+                // Toast 通知
+                Toast.MakeText(this, $"You Clicked {count}", ToastLength.Short).Show();
+            };
+        }
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+	}
+}
+
